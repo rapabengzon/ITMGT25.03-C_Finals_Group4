be5bd6e63fd7861120f50b9084351c2ab6b5f875
rapabengzon <135810149+rapabengzon@users.noreply.github.com>
2023-07-12 11:36:53 +0800

    july 12 consultation

--- store/urls.py
+++ store/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'store'
 
 urlpatterns = [
-    path('', views.all_products, name='all_products')    
+    path('', views.all_products, name='all_products'),
+    path('products', views.products, name='products')
 ]
--- store/views.py
+++ store/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render
 from .models import Category, Product
 
 def all_products(request):
-    products = Product.objects.all()
-    return render(request, 'store/home.html', {'products': products})
+    #products = Product.objects.all()
+    return render(request, 'store/home.html') 
+                  #{'products': products})
+
+def products(request):
+    return render(request, "store/products.html")
