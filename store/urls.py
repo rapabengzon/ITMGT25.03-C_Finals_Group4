dfbc3e75163c9a7ba6cc8da3803d1d9e15ac391d
rapabengzon <135810149+rapabengzon@users.noreply.github.com>
2023-07-15 14:13:10 +0800

    second commit

--- store/urls.py
+++ store/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'store'
 
 urlpatterns = [
-    path('', views.all_products, name='all_products'),
-    path('products', views.products, name='products')
+    path('', views.home, name='home'),
+    path("item/<slug:slug>", views.product_detail, name="product_detail"),
+    path('shop/<slug:category_slug>/', views.category_list, name='category_list'),
 ]
