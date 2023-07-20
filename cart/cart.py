1d1be82adaca3e265a1a4b4cad88008faaab0197
rapabengzon <135810149+rapabengzon@users.noreply.github.com>
2023-07-21 01:27:46 +0800

    Updated cart functionalities

--- cart/cart.py
+++ cart/cart.py
@@ -11,36 +11,47 @@ class Cart():
             cart = self.session['skey'] = {}
         self.cart = cart
 
-    def add(self, product):
+    def add(self, product, qty):
         product_id = product.id
-        if product_id not in self.cart:
-            self.cart[product_id] = {'price': int(product.price)}
+        if product_id in self.cart:
+            self.cart[product_id]['qty'] = qty
+        else:
+            self.cart[product_id] = {'price': str(product.price), 'qty': qty}
 
-        self.session.modified = True
-
-    # def add(self, product, qty):
-    #     product_id = str(product.id)
-    #     if product_id in self.cart:
-    #         self.cart[product_id]['qty'] = qty
-    #     else:
-    #         self.cart[product_id] = {'price': str(product.price), 'qty': qty}
-    #     self.save()
-
-    # def save(self):
-    #     self.session.modified = True
-
-    # def __iter__(self):
-    #     product_ids = self.cart.keys()
-    #     products = Product.products.filter(id__in=product_ids)
-    #     cart = self.cart.copy()
-
-    #     for product in products:
-    #         cart[str(product.id)]['product'] = product
-
-    #     for item in cart.values():
-    #         item['price'] = Decimal(item['price'])
-    #         item['total_price'] = item['price'] * item['qty']
-    #         yield item
+        self.save()
     
-    # def __len__(self):
-    #     return sum(item['qty'] for item in self.cart.values())
+    def __iter__(self):
+        product_ids = self.cart.keys()
+        products = Product.products.filter(id__in=product_ids)
+        cart = self.cart.copy()
+
+        for product in products:
+            cart[str(product.id)]['product'] = product
+
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['qty']
+            yield item
+    
+    def __len__(self):
+        return sum(item['qty'] for item in self.cart.values())
+    
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['qty'] for item in self.cart.values())
+    
+    def delete(self, product):
+        product_id = str(product)
+
+        if product_id in self.cart:
+            del self.cart[product_id]
+            print(product_id)
+            self.save()
+
+    def update(self, product, qty):
+        product_id = str(product)
+        if product_id in self.cart:
+            self.cart[product_id]['qty'] = qty
+        self.save()
+
+    def save(self):
+        self.session.modified = True
--- cart/views.py
+++ cart/views.py
@@ -8,18 +8,40 @@ from .cart import Cart
 # Create your views here.
 
 def cart_summary(request):
-    return render(request, 'cart/summary.html')
+    cart = Cart(request)
+    return render(request, 'cart/summary.html', {'cart': cart})
 
 def cart_add(request):
     cart = Cart(request)
     if request.POST.get('action') == 'post':
         product_id = int(request.POST.get('productid'))
-        # product_qty = int(request.POST.get('productqty'))
+        product_qty = int(request.POST.get('productqty'))
         product = get_object_or_404(Product, id=product_id)
-        cart.add(product=product)
-        response = JsonResponse({'test':'data'})
+        cart.add(product=product, qty=product_qty)
+
+        cartqty = cart.__len__()
+        response = JsonResponse({'qty': cartqty})
         return response
-        # cart.add(product=product, qty=product_qty)
-        # cartqty = cart.__len__()
-        # response = JsonResponse({'qty': cartqty})
-        # return response
+    
+def cart_delete(request):
+    cart = Cart(request)
+    if request.POST.get('action') == 'delete':
+        product_id = int(request.POST.get('productid'))
+        cart.delete(product=product_id)
+
+        cartqty = cart.__len__()
+        carttotal = cart.get_total_price()
+        response = JsonResponse({'qty': cartqty, 'subtotal': carttotal})
+        return response
+    
+def cart_update(request):
+    cart = Cart(request)
+    if request.POST.get('action') == 'update':
+        product_id = int(request.POST.get('productid'))
+        product_qty = int(request.POST.get('productqty'))
+        cart.update(product=product_id, qty=product_qty)
+
+        basketqty = cart.__len__()
+        baskettotal = cart.get_total_price()
+        response = JsonResponse({'qty': basketqty, 'subtotal': baskettotal})
+        return response
